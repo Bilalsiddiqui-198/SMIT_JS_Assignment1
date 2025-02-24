209bb23d3c0f2eed2668297b7e9ebaeb97e85497
Bilalsiddiqui-198 <siddiqui360bilal@gmail.com>
2025-02-24 17:49:36 +0500

    Add files via upload

---
+++ chapter5/script.js
@@ -0,0 +1,30 @@
+//task1
+
+var num1=2;
+var num2=5;
+var sum=num1+num2;
+document.write("sum of "+num1+" and "+num2+" is "+sum+"<br><br>");
+
+
+//task2
+var sub=num1-num2;
+document.write("subtraction of "+num1+" and "+num2+" is "+sub+"<br>");
+var mul=num1*num2;
+document.write("multiplicaion of "+num1+" and "+num2+" is "+mul+"<br>");
+var div=num2/num1;
+document.write("division of "+num1+" and "+num2+" is "+div+"<br><br><br>");
+
+//task3
+
+var number;
+document.write("Value after variable declaration is: " + number+"<br>");
+number = 5;
+document.write("Initial value: " + number+"<br>");
+number++;
+document.write("Value after increment is: " + number+"<br>" );
+number += 7;
+document.write("Value after addition is: " + number+"<br>" );
+number--;
+document.write("Value after decrement is: " + number+"<br>" );
+var remainder = number % 3;
+document.write("The remainder is: " + remainder+"<br>" );
